fdc4e145ec4604b134355ba40f7111d631381078
sexfat <bryant6831@gmail.com>
2021-06-25 19:56:18 +0800

    add

--- tasks/sassStyle.js
+++ tasks/sassStyle.js
@@ -4,6 +4,7 @@ const {
 } = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 //把路徑載入
 const config = require('../gulp.config')
@@ -11,10 +12,12 @@ const config = require('../gulp.config')
 
 module.exports = function styleSass() {
     return src(config.paths.css)
+        .pipe(sourcemaps.init())
         .pipe(sass({
-            outputStyle: "compressed" //壓縮css  expanded
+            outputStyle: "expanded" //壓縮css  expanded
         }).on('error', sass.logError))
         .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
         .pipe(dest(config.pathsDev.css))
 }
 
