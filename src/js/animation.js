1a6c45fe0f296bc3593952a8bb7855a942b4baa4
sexfat <bryant6831@gmail.com>
2021-07-04 17:28:04 +0800

    draft 版完成

--- src/js/animation.js
+++ src/js/animation.js
@@ -7,19 +7,42 @@ gsap.to(".box", {
 
   // slider
 
-  const swiper = new Swiper('.swiper-container', {
-    
+  const swiper = new Swiper('.silder_1', {
     loop: true,
-
-  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
-
-
-
-
   });
+
+
+  const swipers = new Swiper('.silder_2', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  const swiper_ig = new Swiper('.silder_3', {
+    loop: true,
+    slidesPerView: 6,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+
+
+
+
+ 
   
