2e9f805bfb9df50bba1bcc5f7399fbf6f1c7084c
sexfat <bryant6831@gmail.com>
2021-07-17 08:43:54 +0800

    add

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,62 +1,68 @@
 // navigation
 
-
-
-
-
-function call_menu() {
-
+function navmenu() {
     var menu = new MmenuLight(
         document.querySelector('#navigation'),
         'all'
     );
     var navigator = menu.navigation({
         // selectedClass: 'Selected',
-        // slidingSubmenus: true,
+        slidingSubmenus: true,
         // theme: 'dark',
-        // title: 'Menu'
+        title: 'Mary Kay'
     });
 
     var drawer = menu.offcanvas({
-        // position: 'left'
+        position: 'left'
     });
 
 
-    // //	Open the menu.
+    //Open the menu.
     document.querySelector('.ham_bar')
         .addEventListener('click', event => {
             event.preventDefault();
             drawer.open();
         });
+
 }
+
 
 
 //判斷
+
+
+
+
+
+
+function rwd(mq) {
+    if (mq.matches) {
+        console.log('>768 桌機版');
+        moblieclose();
+    } else {
+        console.log('<767手機版');
+        navmenu();
+    }
+}
+
 const mq = window.matchMedia("(min-width: 768px)");
+rwd(mq);
+mq.addListener(rwd, false);
 
 
 
-if (mq.matches) {
-    console.log('>768 桌機版');
-} else {
-    console.log('>767手機版');
-    call_menu();
+function moblieclose() {
+    var resizeId;
+    window.addEventListener('resize', function () {
+        clearTimeout(resizeId);
+        resizeId = setTimeout(doneResizing, 500);
+    });
+
+    function doneResizing() {
+        //whatever we want to do
+        window.location.reload();
+    }
 }
 
 
 
-
-// const smallDevice = window.matchMedia("(min-width: 768px)");
-
-// smallDevice.addListener(handleDeviceChange);
-
-// function handleDeviceChange(e) {
-//     if (e.matches) {
-//         console.log('桌機版')
-//     } else {
-//         call_menu();
-//     };
-// }
-
-// // Run it initially
-// handleDeviceChange(smallDevice);
