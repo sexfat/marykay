9e8af259522264d54e02df2b71cf4756aa94bfd7
kcc7907 <kcc7907@gmail.com>
2021-07-25 20:07:49 +0800

    feat: privacy > text to i18n

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -5,21 +5,19 @@
       <div class="row">
         <div class="breadcrumbs margin-top_20">
           <nuxt-link :to="localePath('/')">{{ $t('pages.product_page.home') }}</nuxt-link> /
-          {{ $t('pages.global_privacy.page_name') }}
+          {{ $t('pages.privacy.page_name') }}
         </div>
       </div>
     </div>
     <div class="container white">
-      <h3 class="h3">{{ $t('pages.global_privacy.page_name') }}</h3>
-
+      <h3 class="h3">{{ $t('pages.privacy.page_name') }}</h3>
       <div class="privacy_inner">
-        <div class="text_inner margin-bottom_50" v-html="$t('pages.global_privacy.text_inner')"></div>
         <div v-for="(value, key) in contentData" :key="key">
           <a :href="`#${key}`" class="accordion-toggle"
-            >{{ $t(`pages.global_privacy.contents.${key}.name`) }}
+            >{{ $t(`pages.privacy.contents.${key}.name`) }}
             <span class="arrow_down"><img src="~assets/images/down-arrow.svg" alt="" /></span
           ></a>
-          <div :id="`${key}`" class="accordion-content" v-html="$t(`pages.global_privacy.contents.${key}.text`)"></div>
+          <div :id="`${key}`" class="accordion-content" v-html="$t(`pages.privacy.contents.${key}.text`)"></div>
         </div>
       </div>
     </div>
@@ -28,10 +26,10 @@
 
 <script>
 export default {
-  name: 'GlobalPrivacy',
+  name: 'Privacy',
   computed: {
     contentData() {
-      return this.$i18n.messages[this.$i18n.locale].pages.global_privacy.contents
+      return this.$i18n.messages[this.$i18n.locale].pages.privacy.contents
     },
   },
 }
