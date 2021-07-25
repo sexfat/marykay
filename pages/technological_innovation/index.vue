dfd7b9df3c121f10e20b479441bdbf530af16c86
nimo <>
2021-07-25 22:04:25 +0800

    json調整

---
+++ pages/technological_innovation/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tech">
+    <div class="container-xxl">
+      <div class="row">
+        <div class="breadcrumbs margin-top_20"><a href="">首頁</a> / 關於Mary Kay / 科研創新</div>
+      </div>
+    </div>
+
+    <div class="banner container-fluid gx-0">
+      <img src="~assets/images/HK_layoutA/tech_innov-top.jpg" alt="科研創新" />
+    </div>
+    <div class="tech_inner container-xxl white">
+      <div class="row">
+        <div class="col-xs-6col-md-6 text_head">
+          <img style="width: 70px" src="~assets/images/com_colorLine.png" alt="" />
+          <h2 class="h2 font1 margin-top_20">{{ $t('pages.technological_innovation.title') }}</h2>
+
+          <div class="text_body">
+            <ul>
+              <li>超過200項優質產品，包括護膚品、彩妝、身體護理、防曬、香水及營養保健品。</li>
+              <li>超過200位來自各領域科學家的研發團隊</li>
+              <li>擁有1,500＋專利項目</li>
+              <li>每年耗資數百萬美元研發</li>
+              <li>每年超過50萬次試驗確保產品安全性及功效</li>
+              <li>
+                投資超過一億美元建設占地超過42000平方米的R3全球製造與研發中心，擁有先進的研發實驗室、尖端的製造技術，還擁有全新的現代化的零污染填埋設施。
+              </li>
+              <li>超過10次獲得 #Beauty 20 award 國際美妝大賞，被視為最具創新品牌</li>
+              <li>多項產品獲得#Goodhousekeeping 國際美妝雜誌標章認可</li>
+            </ul>
+          </div>
+        </div>
+        <!-- <div class="row text_center px-2 margin-top_80">
+                        ...
+
+                    </div> -->
+      </div>
+    </div>
+    <div class="tech_content container-xxl white margin-bottom_150">
+      <div class="row">
+        <div class="best col-xs-12 col-md-4">
+          <img src="~assets/images/HK_layoutA/tech_invo-02.png" alt="" />
+          <div class="desc m35">美國達拉斯R3實驗室</div>
+        </div>
+        <div class="best col-xs-12 col-md-4">
+          <img src="~assets/images/HK_layoutA/tech_invo-03.jpg" alt="" srcset="" />
+          <div class="desc">多項產品獲得Goodhousekeeping 國際美妝雜誌標章認可。</div>
+        </div>
+        <div class="best col-xs-12 col-md-4">
+          <img src="~assets/images/HK_layoutA/tech_invo-04.jpg" alt="" />
+          <div class="desc">超過10次獲得Beauty 20 award國際美妝大賞，被視為最具創新品牌。</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
