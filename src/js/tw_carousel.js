bf533966863faee46fd091524c19074b61c765a8
sexfat <bryant6831@gmail.com>
2021-07-27 08:57:40 +0800

    台灣頁面

---
+++ src/js/tw_carousel.js
@@ -0,0 +1,72 @@
+// 創辦人故事 
+const swiper_story = new Swiper(['.swiper_story' , '.swiper_pink'], {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    // autoplay: {
+    //     delay: 3000,
+    // },
+    // breakpoints: {
+    //     320: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 0,
+    //         // slidesPerView: "auto",
+    //         // centeredSlides: true,
+    //         // centeredSlides: true,
+    //     },
+    //     769: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 15,
+    //     }
+    // },
+
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// 粉紅色公益
+// const swiper_pink = new Swiper('.swiper_pink', {
+//     loop: true,
+//     slidesPerView: 1,
+//     spaceBetween: 0,
+//     // autoplay: {
+//     //     delay: 3000,
+//     // },
+//     navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+
+
+// //環境保護 slider
+
+
+
+// const silder_ev = new Swiper('.silder_ev', {
+//     loop: true,
+//     slidesPerView: 2,
+//     spaceBetween: 20,
+//     autoplay: {
+//         delay: 2500,
+//     },
+//     breakpoints: {
+//         320: {
+//             slidesPerView: 1,
+//             spaceBetween: 15,
+//             // slidesPerView: "auto",
+//             // centeredSlides: true,
+//             // centeredSlides: true,
+//         },
+//         769: {
+//             slidesPerView: 2,
+//             spaceBetween: 20,
+//         }
+//     },
+
+//     // Navigation arrows
+// });
