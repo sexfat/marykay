21942ed9352d7ef2cc99ea18e077023083ec625d
kcc7907 <kcc7907@gmail.com>
2021-08-01 01:54:32 +0800

    fix: products > change banner_title data from zh.json

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -5,7 +5,7 @@
     <div class="product_banner container-fluid gx-0">
       <img :src="require(`~/assets/images/HK_prodBanner/${bannerImgSrc}`)" alt="" />
       <h1 class="title container-xxl" :class="{ '-black': $i18n.messages['zh'].pages.series_page.series[id].black }">
-        <div class="st">{{ $i18n.messages['en'].pages.series_page.series[id].banner_title }}</div>
+        <div class="st">{{ $i18n.messages['zh'].pages.series_page.series[id].banner_title }}</div>
         {{ $i18n.messages['zh'].pages.series_page.series[id].name }}
       </h1>
     </div>
