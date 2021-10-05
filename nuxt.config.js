2329063701bed2be8e34c4eddaae9a4f3b8c7d11
sexfat <bryant6831@gmail.com>
2021-10-05 14:18:11 +0800

    刪除gta

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,19 +65,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['nuxt-i18n', '@nuxtjs/google-gtag'],
-  'google-gtag': {
-    id: 'G-8JYB10FRYJ', // required
-    config: {
-      // this are the config options for `gtag
-      // check out official docs: https://developers.google.com/analytics/devguides/collection/gtagjs/
-      anonymize_ip: true,
-      send_page_view: false,
-    },
-    debug: true, // enable to track in dev mode
-    disableAutoPageTrack: false,
-  },
-
+  modules: ['nuxt-i18n'],
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ['gsap'],
