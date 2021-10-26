e80412282ac506da80dddeeb91cb32aff30fb696
kcc7907 <kcc7907@gmail.com>
2021-10-26 21:40:39 +0800

    fix: nuxtConfig > add gtm dangerouslyDisableSanitizersByTagID

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,9 @@ export default {
         innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WF4FTJW');`,
       },
     ],
+    __dangerouslyDisableSanitizersByTagID: {
+      gtm: ['innerHTML'],
+    },
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
