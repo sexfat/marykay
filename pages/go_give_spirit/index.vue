0e4f45532c06860c50ceb0919f73d533cdf8728d
sexfat <bryant6831@gmail.com>
2023-05-04 21:37:50 +0800

    add

--- pages/go_give_spirit/index.vue
+++ pages/go_give_spirit/index.vue
@@ -37,7 +37,7 @@
 
       <section class="spirit__slider">
         <Swiper ref="swiper" :options="swiperOptions">
-          <Swiper-slide v-for="item in 6" :key="item">
+          <Swiper-slide v-for="item in 10" :key="item">
             <img :src="require(`~/assets/images/spirit/slide_${item}.jpg`)" :alt="`spirit slide_${item}`" />
           </Swiper-slide>
         </Swiper>
